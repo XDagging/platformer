e626e6d5d1b1fadb09c6be41cb26a971403232c2
XDagging <113935822+XDagging@users.noreply.github.com>
2025-05-21 12:19:30 +0000

    finished project

--- workspace/src/gamelogic/level/Level.java
+++ workspace/src/gamelogic/level/Level.java
@@ -197,7 +197,66 @@ public class Level {
 	//Your code goes here! 
 	//Please make sure you read the rubric/directions carefully and implement the solution recursively!
 	private void water(int col, int row, Map map, int fullness) {
+		Water w = null;
+		// Water is the initial starting point and I have to handle flowing
+		// Water w = new Water (col, row, tileSize, tileset.getImage("Full_water"), this, fullness);
+		// map.addTile(col, row, w);
 		
+		// check if we can go down
+		// if (row+1 < map.getTiles()[col].length &&map.getTiles()[col][row+1].isSolid()) {
+		// 	w = new Water (col, row, tileSize, tileset.getImage("Full_water"), this, 3);
+		// } else {
+		if (fullness == 3) {
+			w = new Water (col, row, tileSize, tileset.getImage("Full_water"), this, fullness);
+		} else if (fullness == 2) {
+			w = new Water (col, row, tileSize, tileset.getImage("Half_water"), this, fullness);
+		} else if (fullness == 1) {
+			w = new Water (col, row, tileSize, tileset.getImage("Quarter_water"), this, fullness);
+		} else if (fullness ==0) {
+			w = new Water (col, row, tileSize, tileset.getImage("Falling_water"), this, fullness);
+		}
+
+
+		
+
+
+		
+		map.addTile(col,row,w);
+
+                       //if we can’t go down go left and right.
+		//right
+		int newFullness = (fullness-1 >= 1) ? fullness - 1 : 1;
+
+		if (row+1 < map.getTiles()[col].length && !(map.getTiles()[col][row+1] instanceof Water) && (!map.getTiles()[col][row+1].isSolid())) {
+			
+			// if (map.getTiles()[col][row]) {}
+				if (row+2 < map.getTiles()[col].length&&map.getTiles()[col][row+2].isSolid()) {
+					water(col, row+1, map, 3);
+				}else{
+					water(col, row+1, map, 0);
+				}
+			} else if ((row+1<map.getTiles()[col].length)&&(map.getTiles()[col][row+1].isSolid()) ) {
+				if(col+1 < map.getTiles().length && !(map.getTiles()[col+1][row] instanceof Water)) {
+					water(col+1, row, map, newFullness);
+				}
+			//left
+				if(col-1 >= 0 && !(map.getTiles()[col-1][row] instanceof Water)) {
+					water(col-1, row, map, newFullness);
+				}
+		
+		}
+
+
+			
+		
+		// down
+
+		
+
+
+
+
+
 	}
 
 
